6fa0b537b7a76463213f047324270f359ed065a3
wuyawei <wuyawei.mail@gmail.com>
2012-11-06 13:30:19 +0800

    m

--- src/main/java/com/my/test/rabbitmq/Test.java
+++ src/main/java/com/my/test/rabbitmq/Test.java
@@ -5,34 +5,78 @@ import java.io.IOException;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.ConsumerCancelledException;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.ShutdownSignalException;
 
 public class Test {
 
-//	private final static String QUEUE_NAME = "hello";
+	private final static String QUEUE_NAME = "notifications.queue";
 
-	/**
-	 * 
-	 * @param args
-	 * @throws IOException
-	 */
-	public static void main(String[] args) throws IOException {
+	private static final String EXCHANGE_NAME = "nova";
+
+	public static void main(String[] args) throws Exception {
+
+		test2();
+
+	}
+
+	public static void test1() throws IOException, ShutdownSignalException,
+			ConsumerCancelledException, Exception {
 
 		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost("localhost");
+		factory.setUsername("guest");
+		factory.setPassword("password");
+		factory.setVirtualHost("/");
+		factory.setHost("192.168.122.203");
+		factory.setPort(5672);
+
 		Connection connection = factory.newConnection();
 		Channel channel = connection.createChannel();
 
-		
-//		channel.exchangeDeclare("logs", "fanout");
-//		
-//		
-//		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-//		String message = "Hello World!";
-//		channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-//		System.out.println(" [x] Sent '" + message + "'");
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-		channel.close();
-		connection.close();
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			System.out.println(" [x] Received '" + message + "'");
+		}
+
+	}
+
+	public static void test2() throws IOException, ShutdownSignalException,
+			ConsumerCancelledException, Exception {
+
+		ConnectionFactory factory = new ConnectionFactory();
+		factory.setUsername("guest");
+		factory.setPassword("guest");
+		factory.setVirtualHost("/");
+		factory.setHost("10.128.5.13");
+		factory.setPort(5672);
+
+		Connection connection = factory.newConnection();
+		Channel channel = connection.createChannel();
+
+		channel.exchangeDeclarePassive(EXCHANGE_NAME);
+
+		String queueName = channel.queueDeclare().getQueue();
+		channel.queueBind(queueName, EXCHANGE_NAME, "notifications");
+
+		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+		channel.basicConsume(queueName, true, consumer);
+
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+
+			System.out.println(" [x] Received '" + message + "'");
+		}
 
 	}
 
